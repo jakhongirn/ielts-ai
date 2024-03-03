4f6030fe7f17b8b8086e5071fadca2a8de888a0b
Jakhongir <jahongirnagmatov@gmail.com>
2024-03-03 21:59:14 +0500

    add UserProfile model and automatically create it by creating User

--- django/authentication/models.py
+++ django/authentication/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserManager(BaseUserManager):
@@ -48,3 +50,25 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=50, null=True, blank=True)
+    date_birth = models.DateField(null=True, blank=True)
+    updated_at = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return f'{self.user.email} Profile'
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+        
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
