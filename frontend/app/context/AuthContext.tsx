b92adf2d93dae8e00cb6968b089576591285c7fe
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2024-05-02 20:02:02 +0500

    fix Authcontext

--- frontend/app/context/AuthContext.tsx
+++ frontend/app/context/AuthContext.tsx
@@ -6,8 +6,6 @@ import React, {
     ReactNode,
 } from "react";
 import axios from "axios";
-import Cookies from "js-cookie";
-
 interface User {
     username: string;  // Example property, add more based on your user object structure
     email: string;
@@ -33,24 +31,20 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     useEffect(() => {
-        const token = Cookies.get('token');
+        const token = window.sessionStorage.getItem('token')
         if (token) {
-          axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-          fetchUserDetails(); // Fetch user details on initial load
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            fetchUserDetails(); // Fetch user details on initial load
         }
-        
-      },[]);
+      }, []);
     
       const fetchUserDetails = async () => {
         try {
-          const response = await axios.get('/api/user');
+          const response = await axios.get(`${process.env.NEXT_PUBLIC_API}/api/user/`);
           setUser(response.data);
           setIsAuthenticated(true);
-          console.log(isAuthenticated)
-          console.log(user)
         } catch (error) {
           console.error('Error fetching user details:', error);
-          Cookies.remove('token');
         }
       };
 
@@ -61,7 +55,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
                 { email, password }
             );
             const { access } = response.data;
-            Cookies.set("token", access, { expires: 7 }); // Expires in 1 day
+            window.sessionStorage.setItem('token', access)
             await fetchUserDetails();
         } catch (error) {
             console.error("Login failed:", error);
@@ -69,7 +63,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     };
 
     const logout = () => {
-        Cookies.remove("token");
+        window.sessionStorage.removeItem('token')
         setUser(null);
         setIsAuthenticated(false);
         delete axios.defaults.headers.common["Authorization"];
