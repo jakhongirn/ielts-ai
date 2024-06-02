2279a1b241e2e64fc34386304b201eeadad57c0e
ulugbekamanov00 <ulugbekamanov00@gmail.com>
2024-06-03 00:41:52 +0300

    added UserMockTest, UserTestResult, CorrectAnswers to admin.py

--- backend/mocktest/admin.py
+++ backend/mocktest/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import UserPackage, Package, MockTest
+from .models import UserPackage, Package, MockTest, UserMockTest, UserTestResult, CorrectAnswers
 from .forms import MockTestForm
 
 # Register your models here.
 
 class UserPackageAdmin(admin.ModelAdmin):
+    pass
+class UserMockTestAdmin(admin.ModelAdmin):
+    pass
+class UserTestResultAdmin(admin.ModelAdmin):
+    pass
+class CorrectAnswersAdmin(admin.ModelAdmin):
     pass
 
 class PackageAdmin(admin.ModelAdmin):
@@ -18,3 +24,6 @@ class MockTestAdmin(admin.ModelAdmin):
 admin.site.register(UserPackage, UserPackageAdmin)
 admin.site.register(Package, PackageAdmin)
 admin.site.register(MockTest, MockTestAdmin)
+admin.site.register(UserMockTest, UserMockTestAdmin)
+admin.site.register(UserTestResult, UserTestResultAdmin)
+admin.site.register(CorrectAnswers, CorrectAnswersAdmin)
