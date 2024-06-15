cc91fe27d377aba9338d90a8b7f0a47aff2ed69f
Jakhongir <jahongirnagmatov@gmail.com>
2024-06-15 13:07:08 +0500

    cover /dashboard urls to ask token before accessing (authentication)

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,17 +1,20 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+
 export function middleware(request: NextRequest) {
-    const accessToken = request.cookies.get("accessToken"); 
-    // Protect specific routes like /profile
-    if (!accessToken && request.nextUrl.pathname !== "/auth/login") {
+    const accessToken = request.cookies.get("accessToken");
+    
+    // Redirect to login if accessToken is not present and trying to access /dashboard
+    if (!accessToken && request.nextUrl.pathname.startsWith("/dashboard")) {
         return NextResponse.redirect(new URL("/auth/login", request.url));
-      }
+    }
+
+
+
     return NextResponse.next();
 }
 
 export const config = {
     matcher: ["/dashboard/:path*"]
-    // Apply this middleware only to the /profile route
 };
